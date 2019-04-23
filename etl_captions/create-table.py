4a75fdd4423f80adab959571288a69ae2fcde5ac
Nilesh Malpekar <nileshmalpekar@gmail.com>
2019-04-22 22:49:46 -0500

    PEP8 formatted

--- etl_captions/create-table.py
+++ etl_captions/create-table.py
@@ -3,48 +3,52 @@ from os import environ
 
 TABLE_NAME = 'videos'
 
+
 def create_table():
-  dynamodb = boto3.resource('dynamodb',
-    endpoint_url=environ['DYNAMO_ENDPOINT'],
-    region_name=environ['AWS_REGION_NAME'],
-    aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'])
+    dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url=environ['DYNAMO_ENDPOINT'],
+        region_name=environ['AWS_REGION_NAME'],
+        aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'])
 
-  table = dynamodb.create_table(
-    TableName=TABLE_NAME,
-    KeySchema=[
-      {
-        'AttributeName': 'videoId',
-        'KeyType': 'HASH'
-      },
-      {
-        'AttributeName': 'created',
-        'KeyType': 'RANGE'
-      }
-    ],
-    AttributeDefinitions=[
-      {
-        'AttributeName': 'videoId',
-        'AttributeType': 'S'
-      },
-      {
-        'AttributeName': 'created',
-        'AttributeType': 'S'
-      },
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 5,
-        'WriteCapacityUnits': 5
-    }
-  )
+    table = dynamodb.create_table(
+        TableName=TABLE_NAME,
+        KeySchema=[
+            {
+                'AttributeName': 'videoId',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'created',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'videoId',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'created',
+                'AttributeType': 'S'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 5,
+            'WriteCapacityUnits': 5
+        }
+    )
 
-  table.meta.client.get_waiter('table_exists').wait(TableName=TABLE_NAME)
+    table.meta.client.get_waiter('table_exists').wait(TableName=TABLE_NAME)
 
-  print(table.item_count)
-  print("Table %s created" % TABLE_NAME)
+    print(table.item_count)
+    print("Table %s created" % TABLE_NAME)
+
 
 def run():
-  create_table()
+    create_table()
+
 
 if __name__ == '__main__':
-  run()
+    run()
--- etl_captions/main.py
+++ etl_captions/main.py
@@ -16,109 +16,125 @@ VIDEO_URL = 'https://www.youtube.com/watch?v=%s'
 
 LANG = 'es'
 
+
 def get_thumbnail(item):
-  if 'standard' in item['snippet']['thumbnails']:
-    return item['snippet']['thumbnails']['standard']['url']
-  if 'high' in item['snippet']['thumbnails']:
-    return item['snippet']['thumbnails']['high']['url']
-  if 'medium' in item['snippet']['thumbnails']:
-    return item['snippet']['thumbnails']['medium']['url']
-  if 'default' in item['snippet']['thumbnails']:
-    return item['snippet']['thumbnails']['default']['url']
-  return ''
+    if 'standard' in item['snippet']['thumbnails']:
+        return item['snippet']['thumbnails']['standard']['url']
+    if 'high' in item['snippet']['thumbnails']:
+        return item['snippet']['thumbnails']['high']['url']
+    if 'medium' in item['snippet']['thumbnails']:
+        return item['snippet']['thumbnails']['medium']['url']
+    if 'default' in item['snippet']['thumbnails']:
+        return item['snippet']['thumbnails']['default']['url']
+    return ''
+
 
 def youtube_transformer(response):
-  videos = []
-  for item in response['items']:
-    videoId = item['snippet']['resourceId']['videoId']
-    title = item['snippet']['title']
-    created = item['contentDetails']['videoPublishedAt']
-    thumbnail = get_thumbnail(item)
-    print("VideoId:%s,Title:%s" % (videoId, title))
+    videos = []
+    for item in response['items']:
+        videoId = item['snippet']['resourceId']['videoId']
+        title = item['snippet']['title']
+        created = item['contentDetails']['videoPublishedAt']
+        thumbnail = get_thumbnail(item)
+        print("VideoId:%s,Title:%s" % (videoId, title))
 
-    videos.append({
-      'videoId': videoId,
-      'title': title,
-      'created': created,
-      'thumbnail': thumbnail
-    })
+        videos.append({
+            'videoId': videoId,
+            'title': title,
+            'created': created,
+            'thumbnail': thumbnail
+        })
 
-  return videos
+    return videos
 
-def get_videos_from_youtube(pageToken = ''):
-  youtube_url = YOUTUBE_URL % (MAX_RESULTS, PLAYLIST_ID, API_KEY)
 
-  if pageToken:
-    youtube_url += '&pageToken=%s' % pageToken
+def get_videos_from_youtube(pageToken=''):
+    youtube_url = YOUTUBE_URL % (MAX_RESULTS, PLAYLIST_ID, API_KEY)
 
-  response = requests.get(url = youtube_url)
-  return response.json()
+    if pageToken:
+        youtube_url += '&pageToken=%s' % pageToken
 
-def get_videos(step = 0, pageToken = ''):
-  response = get_videos_from_youtube(pageToken)
+    response = requests.get(url=youtube_url)
+    return response.json()
 
-  videos = youtube_transformer(response)
-  next_token = response['nextPageToken'] if 'nextPageToken' in response else False
 
-  if next_token or step < MAX_TRIES:
-    videos += get_videos(step + 1, next_token)
+def get_videos(step=0, pageToken=''):
+    response = get_videos_from_youtube(pageToken)
 
-  return videos
+    videos = youtube_transformer(response)
+    next_token = response['nextPageToken'] if 'nextPageToken' in response else False
+
+    if next_token or step < MAX_TRIES:
+        videos += get_videos(step + 1, next_token)
+
+    return videos
+
 
 def striphtml(data):
-  p = re.compile(r'<.*?>')
-  return p.sub('', data)
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
 
 def get_captions(video):
-  video_url = VIDEO_URL % video['videoId']
+    video_url = VIDEO_URL % video['videoId']
 
-  source = YouTube(video_url)
-  captions = source.captions.get_by_language_code(LANG)
+    source = YouTube(video_url)
+    captions = source.captions.get_by_language_code(LANG)
 
-  if captions and captions.xml_captions:
-    file_name = 'captions/%s_%s.txt' % (video['videoId'], LANG)
-    captions_file = open(file_name, 'w')
-    root = ET.fromstring(captions.xml_captions)
-    for child in root:
-      captions_file.write("%s\t%s\t%s\n" % (child.attrib['start'], child.attrib['dur'], striphtml(child.text)))
-    print("Created caption file %s" % file_name)
-    return True
-  else:
-    return False
+    if captions and captions.xml_captions:
+        file_name = 'captions/%s_%s.txt' % (video['videoId'], LANG)
+        captions_file = open(file_name, 'w')
+        root = ET.fromstring(captions.xml_captions)
+        for child in root:
+            captions_file.write(
+                "%s\t%s\t%s\n" %
+                (child.attrib['start'],
+                 child.attrib['dur'],
+                 striphtml(
+                    child.text)))
+        print("Created caption file %s" % file_name)
+        return True
+    else:
+        return False
+
 
 def save_videos(videos):
-  dynamodb = boto3.resource('dynamodb',
-    endpoint_url=os.environ['DYNAMO_ENDPOINT'],
-    region_name=os.environ['AWS_REGION_NAME'],
-    aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    dynamodb = boto3.resource(
+        'dynamodb',
+        endpoint_url=os.environ['DYNAMO_ENDPOINT'],
+        region_name=os.environ['AWS_REGION_NAME'],
+        aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+        aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
 
-  table = dynamodb.Table('videos')
-  new_videos_count = 0
+    table = dynamodb.Table('videos')
+    new_videos_count = 0
 
-  for video in videos:
-    response = table.query(
-      KeyConditionExpression=Key('videoId').eq(video['videoId'])
-    )
-    if response['Count'] == 0:
-      if get_captions(video):
-        table.put_item(TableName='videos',Item=video)
-        new_videos_count += 1
+    for video in videos:
+        response = table.query(
+            KeyConditionExpression=Key('videoId').eq(video['videoId'])
+        )
+        if response['Count'] == 0:
+            if get_captions(video):
+                table.put_item(TableName='videos', Item=video)
+                new_videos_count += 1
 
-  return new_videos_count
+    return new_videos_count
+
 
 def create_directory():
-  directory_name = 'captions'
-  if not os.path.exists(directory_name):
-    os.makedirs(directory_name)
+    directory_name = 'captions'
+    if not os.path.exists(directory_name):
+        os.makedirs(directory_name)
+
 
 def run():
-  videos = get_videos()
-  print("Total %s videos found ..." % len(videos))
+    videos = get_videos()
+    print("Total %s videos found ..." % len(videos))
 
-  create_directory()
-  new_videos_count = save_videos(videos)
-  print("Total %s new videos found with captions ..." % new_videos_count)
+    create_directory()
+    new_videos_count = save_videos(videos)
+    print("Total %s new videos found with captions ..." % new_videos_count)
+
 
 if __name__ == '__main__':
-  run()
+    run()
