dd8ee9b3dd223548a49895fd020b965c29b5b2e2
Nilesh Malpekar <nileshmalpekar@gmail.com>
2019-05-04 17:10:32 -0500

    REST api to query labels

--- build_docker_image.sh
+++ build_docker_image.sh
@@ -3,3 +3,6 @@ docker build --rm -f docker/Dockerfile.etl -t cs498_etl:latest docker
 
 # build python data science docker image
 docker build --rm -f docker/Dockerfile.ds -t cs498_ds:latest docker
+
+# build python data science docker image
+docker build --rm -f docker/Dockerfile.api -t cs498_api:latest docker
