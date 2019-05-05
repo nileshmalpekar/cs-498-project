4fb66538ea676b00990858b52cd28bfa0a0d278a
Nilesh Malpekar <nileshmalpekar@gmail.com>
2019-05-05 01:32:58 -0500

    Support for Cloud based DynamoDB

--- etl_captions/upload_labels.py
+++ etl_captions/upload_labels.py
@@ -15,12 +15,17 @@ def run():
 		print("Usage: ./upload_labels.py [filename]")
 		sys.exit(-1)
 
-	dynamodb = boto3.resource(
-		'dynamodb',
-		endpoint_url=environ['DYNAMO_ENDPOINT'],
-		region_name=environ['AWS_REGION_NAME'],
-		aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
-		aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'])
+	if 'DYNAMO_ENDPOINT' in environ:
+		dynamodb = boto3.resource(
+			'dynamodb',
+			endpoint_url=environ['DYNAMO_ENDPOINT'],
+			region_name=environ['AWS_REGION_NAME'])
+	else:
+		dynamodb = boto3.resource(
+			'dynamodb',
+			region_name=environ['AWS_REGION_NAME'],
+			aws_access_key_id=environ['AWS_ACCESS_KEY_ID'],
+			aws_secret_access_key=environ['AWS_SECRET_ACCESS_KEY'])
 
 	table = dynamodb.Table('video_tags')
 
--- upload_labels.sh
+++ upload_labels.sh
@@ -1,2 +1,20 @@
 # create dynamodb tables
-docker-compose run --rm etl python ./etl_captions/upload_labels.py ./output.csv
+if [ "$#" -eq 0 ]; then
+	docker-compose run \
+		--rm \
+		etl \
+		python ./etl_captions/upload_labels.py ./output.csv
+elif [ "$#" -eq 2 ]; then
+	docker run \
+		--rm \
+		-e AWS_REGION_NAME=us-east-1 \
+		-e AWS_ACCESS_KEY_ID=$1 \
+		-e AWS_SECRET_ACCESS_KEY=$2 \
+		-it \
+		-v ${PWD}:/app \
+		-w /app \
+		cs498_etl:latest \
+		python ./etl_captions/upload_labels.py ./output.csv
+else
+	echo "Usage: ./upload_labels.sh [[AWSAccessKeyId] [AWSSecretKey]]"
+fi
